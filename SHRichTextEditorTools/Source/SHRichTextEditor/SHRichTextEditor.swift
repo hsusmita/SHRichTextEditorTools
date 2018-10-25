3dc0663da352c2b8e7aaa6138b41bb9fb3b6d0c0
hsusmita <susmita.horrow@gmail.com>
2018-10-25 08:54:32 +0530

    fix the issue of image not found

--- SHRichTextEditorTools/Source/SHRichTextEditor/SHRichTextEditor.swift
+++ SHRichTextEditorTools/Source/SHRichTextEditor/SHRichTextEditor.swift
@@ -24,15 +24,54 @@ open class SHRichTextEditor: NSObject, RichTextEditor {
 	private var defaultImageInputHandler: ImageInputHandler
 	private var defaultLinkInputHandler: LinkInputHandler
 
-	public static let defaultIndentationButton = ToolBarButton.ButtonType.image(image: UIImage(named: "Bullet")!)
-	public static let defaultItalicButton = ToolBarButton.ButtonType.image(image: UIImage(named: "Italic")!)
-	public static let defaultBoldButton = ToolBarButton.ButtonType.image(image: UIImage(named: "Bold")!)
-	public static let defaultLinkButton = ToolBarButton.ButtonType.image(image: UIImage(named: "Link")!)
-	public static let defaultImageButton = ToolBarButton.ButtonType.image(image: UIImage(named: "Picture-Icon")!)
+	public static let defaultBoldButtonType: ToolBarButton.ButtonType = {
+		let bundle = Bundle(for: SHRichTextEditor.classForCoder())
+		if let picureIcon = UIImage(named: "Bold", in: bundle, compatibleWith: nil) {
+			return ToolBarButton.ButtonType.image(image: picureIcon)
+		} else {
+			return ToolBarButton.ButtonType.title(title: "Bold")
+		}
+	}()
+
+	public static let defaultItalicButtonType: ToolBarButton.ButtonType = {
+		let bundle = Bundle(for: SHRichTextEditor.classForCoder())
+		if let picureIcon = UIImage(named: "Italic", in: bundle, compatibleWith: nil) {
+			return ToolBarButton.ButtonType.image(image: picureIcon)
+		} else {
+			return ToolBarButton.ButtonType.title(title: "Italic")
+		}
+	}()
+
+	public static let defaultIndentationButtonType: ToolBarButton.ButtonType = {
+		let bundle = Bundle(for: SHRichTextEditor.classForCoder())
+		if let picureIcon = UIImage(named: "Bullet", in: bundle, compatibleWith: nil) {
+			return ToolBarButton.ButtonType.image(image: picureIcon)
+		} else {
+			return ToolBarButton.ButtonType.title(title: "Indentation")
+		}
+	}()
+
+	public static let defaultLinkButtonType: ToolBarButton.ButtonType = {
+		let bundle = Bundle(for: SHRichTextEditor.classForCoder())
+		if let picureIcon = UIImage(named: "Link", in: bundle, compatibleWith: nil) {
+			return ToolBarButton.ButtonType.image(image: picureIcon)
+		} else {
+			return ToolBarButton.ButtonType.title(title: "Link")
+		}
+	}()
+
+	public static var defaultImageButtonType: ToolBarButton.ButtonType = {
+		let bundle = Bundle(for: SHRichTextEditor.classForCoder())
+		if let picureIcon = UIImage(named: "Picture", in: bundle, compatibleWith: nil) {
+			return ToolBarButton.ButtonType.image(image: picureIcon)
+		} else {
+			return ToolBarButton.ButtonType.title(title: "Picture")
+		}
+	}()
 
 	public init(textView: UITextView,
-		 defaultTintColor: UIColor = .gray,
-		 selectedTintColor: UIColor = UIColor(red: 57/255.0, green: 200/255.0, blue: 129/255.0, alpha: 1)) {
+				defaultTintColor: UIColor = .gray,
+				selectedTintColor: UIColor = UIColor(red: 57/255.0, green: 200/255.0, blue: 129/255.0, alpha: 1)) {
 		self.textView = textView
 		self.toolBarDefaultTintColor = defaultTintColor
 		self.toolBarSelectedTintColor = selectedTintColor
@@ -41,8 +80,8 @@ open class SHRichTextEditor: NSObject, RichTextEditor {
 		super.init()
 	}
 
-	public func boldBarItem(type: ToolBarButton.ButtonType = SHRichTextEditor.defaultBoldButton,
-					 actionOnSelection: ((ToolBarButton, Bool) -> Void)? = nil) -> ToolBarItem {
+	public func boldBarItem(type: ToolBarButton.ButtonType = SHRichTextEditor.defaultBoldButtonType,
+							actionOnSelection: ((ToolBarButton, Bool) -> Void)? = nil) -> ToolBarItem {
 		let defaultAction: ((ToolBarButton, Bool) -> Void) = { [unowned self] (item, isSelected) in
 			item.barButtonItem.tintColor = isSelected ? self.toolBarSelectedTintColor : self.toolBarDefaultTintColor
 		}
@@ -53,8 +92,8 @@ open class SHRichTextEditor: NSObject, RichTextEditor {
 			textViewDelegate: self.textViewDelegate)
 	}
 
-	public func italicBarItem(type: ToolBarButton.ButtonType = SHRichTextEditor.defaultItalicButton,
-					   actionOnSelection: ((ToolBarButton, Bool) -> Void)? = nil) -> ToolBarItem {
+	public func italicBarItem(type: ToolBarButton.ButtonType = SHRichTextEditor.defaultItalicButtonType,
+							  actionOnSelection: ((ToolBarButton, Bool) -> Void)? = nil) -> ToolBarItem {
 		let defaultAction: ((ToolBarButton, Bool) -> Void) = { [unowned self] (item, isSelected) in
 			item.barButtonItem.tintColor = isSelected ? self.toolBarSelectedTintColor : self.toolBarDefaultTintColor
 		}
@@ -65,8 +104,8 @@ open class SHRichTextEditor: NSObject, RichTextEditor {
 			textViewDelegate: self.textViewDelegate)
 	}
 
-	public func indentationBarItem(type: ToolBarButton.ButtonType = SHRichTextEditor.defaultIndentationButton,
-							actionOnSelection: ((ToolBarButton, Bool) -> Void)? = nil) -> ToolBarItem {
+	public func indentationBarItem(type: ToolBarButton.ButtonType = SHRichTextEditor.defaultIndentationButtonType,
+								   actionOnSelection: ((ToolBarButton, Bool) -> Void)? = nil) -> ToolBarItem {
 		let defaultAction: ((ToolBarButton, Bool) -> Void) = { [unowned self] (item, isSelected) in
 			item.barButtonItem.tintColor = isSelected ? self.toolBarSelectedTintColor : self.toolBarDefaultTintColor
 		}
@@ -77,9 +116,9 @@ open class SHRichTextEditor: NSObject, RichTextEditor {
 			textViewDelegate: self.textViewDelegate)
 	}
 
-	public func linkToolBarItem(type: ToolBarButton.ButtonType = SHRichTextEditor.defaultLinkButton,
-						 actionOnSelection: ((ToolBarButton, Bool) -> Void)? = nil,
-						 linkInputHandler: LinkInputHandler? = nil) -> ToolBarItem {
+	public func linkToolBarItem(type: ToolBarButton.ButtonType = SHRichTextEditor.defaultLinkButtonType,
+								actionOnSelection: ((ToolBarButton, Bool) -> Void)? = nil,
+								linkInputHandler: LinkInputHandler? = nil) -> ToolBarItem {
 		let defaultAction: ((ToolBarButton, Bool) -> Void) = { [unowned self] (item, isSelected) in
 			item.barButtonItem.tintColor = isSelected ? self.toolBarSelectedTintColor : self.toolBarDefaultTintColor
 		}
@@ -91,9 +130,9 @@ open class SHRichTextEditor: NSObject, RichTextEditor {
 			textViewDelegate: self.textViewDelegate)
 	}
 
-	public func imageToolBarItem(type: ToolBarButton.ButtonType = SHRichTextEditor.defaultImageButton,
-						  actionOnSelection: ((ToolBarButton, Bool) -> Void)? = nil,
-						  imageInputHandler: ImageInputHandler? = nil) -> ToolBarItem {
+	public func imageToolBarItem(type: ToolBarButton.ButtonType = SHRichTextEditor.defaultImageButtonType,
+								 actionOnSelection: ((ToolBarButton, Bool) -> Void)? = nil,
+								 imageInputHandler: ImageInputHandler? = nil) -> ToolBarItem {
 		let defaultAction: ((ToolBarButton, Bool) -> Void) = { [unowned self] (item, isSelected) in
 			item.barButtonItem.tintColor = isSelected ? self.toolBarSelectedTintColor : self.toolBarDefaultTintColor
 		}
